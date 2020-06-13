068e9fa5f27d36d2593e0aa5286637a0addfb6f0
1994 <vikram.chawan@solvezy.com>
2020-06-13 20:10:27 +0530

    rendering movies

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,38 +1,53 @@
 const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 
-const movies = [];
+movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
 
-  if (movies.length === 0) {
+  if(movies.length === 0) {
     movieList.classList.remove('visible');
     return;
   } else {
     movieList.classList.add('visible');
   }
+
   movieList.innerHTML = '';
 
-  movies.forEach((movie) => {
+  const filteredMovies = !filter 
+    ? movies 
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach(movie => {
     const movieEl = document.createElement('li');
-    movieEl.textContent = movie.info.title;
+    const { info } = movie; // Object destructuring
+    const { title: movieTitle } = info; 
+    let { getFormattedTitle } = movie;
+    getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle() + ' - ';
+    for(const key in info) {
+      if(key != 'title') {
+        text = text + `${key}: ${info[key]}`;
+      }
+    }
+
+    movieEl.textContent = text;
     movieList.append(movieEl);
   });
+
 };
 
 const addMovieHandler = () => {
   const title = document.getElementById('title').value;
   const extraName = document.getElementById('extra-name').value;
   const extraValue = document.getElementById('extra-value').value;
-
-  if (
+  
+  if(
     title.trim() === '' ||
     extraName.trim() === '' ||
-    extraValue.trim() === ''
-  ) {
-    console.log('add movie handler.  ');
-    return;
+    extraValue.trim() === '') {
+      return;
   }
 
   const newMovie = {
@@ -40,11 +55,24 @@ const addMovieHandler = () => {
       title,
       [extraName]: extraValue
     },
-    id: Math.random()
+    id: Math.random(),
+    getFormattedTitle() {
+      return this.info.title.toUpperCase();
+    }
   };
 
   movies.push(newMovie);
+
   renderMovies();
+
+  console.log(movies);
+
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
