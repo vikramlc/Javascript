7a7e170755aa3e64938f9e6f94cf9994a074ae2b
Vikram <vikram.chawan121@gmail.com>
2020-08-16 12:26:09 +0530

    factory functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -24,3 +24,18 @@ function addMoreNumbers(num1, num2) {
 }
 
 console.log(addMoreNumbers(1,2));
+
+// Factory functions
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return amount * tax;
+  }
+
+  return calculateTax;
+}
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateIncomeTaxAmount(200));
