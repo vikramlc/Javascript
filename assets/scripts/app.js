48ff614cf65c7d558c20a679eed5e06dba1583aa
Vikram <vikram.chawan121@gmail.com>
2020-08-20 17:37:09 +0530

    async await error handling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -22,28 +22,57 @@ const setTimer = duration => {
   return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-    .then(posData => {
-      positionData = posData;
-      return setTimer(2000);
-    })
-    .catch(err => {
-      console.log(err);
-      return "Here, we go..";
-    })
-    .then(data => {
-      console.log(data, positionData);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+async function trackUserHandler() {
+  let posData;
+  let timerData;
+
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch(error) {
+    console.log(error);
+  }
+  console.log(timerData, posData);
+
   setTimer(1000).then(() => {
     console.log('Timer done!');
   });
   console.log('Getting position...');
 }
+
+// async function trackUserHandler() {
+//   const posData = await getPosition();
+//   const timerData = await setTimer(2000);
+//   console.log(timerData, posData);
+
+//   setTimer(1000).then(() => {
+//     console.log('Timer done!');
+//   });
+//   console.log('Getting position...');
+// }
+
+// function trackUserHandler() {
+//   let positionData;
+//   getPosition()
+//     .then(posData => {
+//       positionData = posData;
+//       return setTimer(2000);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       return "Here, we go..";
+//     })
+//     .then(data => {
+//       console.log(data, positionData);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+//   setTimer(1000).then(() => {
+//     console.log('Timer done!');
+//   });
+//   console.log('Getting position...');
+// }
 //   navigator.geolocation.getCurrentPosition(
 //     positionData => {
 //       setTimer(2000).then(data => {
